19819a93133f1e9df1d0eb96ad5fc35a7f3f5da1
Ypriom121 <pritomgupta121@gmail.com>
2023-11-06 09:57:43 +0600

    some data added done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_USER);
+
 // mongodb
 
 
@@ -27,13 +27,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+//     await client.connect();
+
+const foodCollection = client.db('foodCollection').collection('food');
+
+app.get('/food',async (req,res)=>{
+ const cursor = foodCollection.find()
+ const result =await cursor.toArray()
+ res.send(result)
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+//     await client.close();
   }
 }
 run().catch(console.dir);
